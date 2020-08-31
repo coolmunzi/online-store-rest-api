a4aeb9a832de342f2cbbd2a7438478c1b641d38c
Munjal Patel <munjal.patel6@gmail.com>
2020-08-31 23:47:53 +0530

    changing app to application in run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from app import app
+from app import app as application
 from db import db
 
-db.init_app(app)
+db.init_app(application)
 @app.before_first_request
 def create_tables():
     db.create_all()
