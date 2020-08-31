588d8cbe225fff86030e4a01c5c64c055db6931e
Munjal Patel <munjal.patel6@gmail.com>
2020-09-01 00:17:25 +0530

    Upgrading python to 3.7.5 in runtime.txt

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from app import app as application
+from app import app
 from db import db
 
-db.init_app(application)
+db.init_app(app)
+
 @app.before_first_request
 def create_tables():
     db.create_all()
